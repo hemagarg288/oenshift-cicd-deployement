110ec8b3e00ac77692c9050828b3cb21e01f996e
kamakra <124426176+kamakra@users.noreply.github.com>
2024-09-13 16:39:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/will', function (req, res) {
     res.send('{ "response": "Hello NICE people" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Hurray!!!, Its working !!" }');
+    res.send('{ "response": " Hurray!!!, Its working -update1 !!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
